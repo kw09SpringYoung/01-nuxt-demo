7aeaa9dbaa6a07a922cf8e42094579fbfc6ed47c
杨春 <43161234+kw09SpringYoung@users.noreply.github.com>
2021-07-04 19:52:22 +0800

    初始化nuxtjs项目

---
+++ pages/about.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        About Page
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'AboutPage',
+
+    components: {  },
+
+    directives: {  },
+
+    data() {
+        return {
+            
+        };
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        Home Page 
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HomePage',
+
+    components: {  },
+
+    directives: {  },
+
+    data() {
+        return {
+            
+        };
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
