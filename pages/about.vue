9729b41ede5589b4e7a5ca4407774f7f9a793ce1
杨春 <43161234+kw09SpringYoung@users.noreply.github.com>
2021-07-05 10:49:43 +0800

    main

--- pages/about.vue
+++ pages/about.vue
@@ -1,18 +1,6 @@
 <template>
     <div>
         <h1>About Page</h1>
-        <!-- a 链接，刷新导航，走服务端渲染 -->
-        <h2>a 链接</h2>
-        <a href="/">首页</a>
-
-        <!-- router-link 导航链接组件，单页面体验 -->
-        <h2>router-link</h2>
-        <router-link to="/">首页</router-link>
-
-        <!-- 编程式导航 -->
-        <h2>编程式导航</h2>
-        <button @click="onClick">首页</button>
-
     </div>
 </template>
 
