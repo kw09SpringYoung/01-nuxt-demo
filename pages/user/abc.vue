d8415e9678fe20dff2b5dfe607d4412ad558d627
杨春 <43161234+kw09SpringYoung@users.noreply.github.com>
2021-07-05 09:36:50 +0800

    02-router

---
+++ pages/user/abc.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <h1>ABC Page</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'UserAbc',
+
+    components: {  },
+
+    directives: {  },
+
+    data() {
+        return {
+            
+        };
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ pages/user/index.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <h1>User Page</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'UserPage',
+
+    components: {  },
+
+    directives: {  },
+
+    data() {
+        return {
+            
+        };
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
