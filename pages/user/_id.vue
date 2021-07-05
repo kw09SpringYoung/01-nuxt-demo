6229c86e359af5acb3528d1a2281e9e9ffab2dd9
杨春 <43161234+kw09SpringYoung@users.noreply.github.com>
2021-07-05 10:52:49 +0800

    04-动态路由

---
+++ pages/user/_id.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <h1>User Page</h1>
+        <p>{{$router.params.id}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'UserPage',
+
+    components: {  },
+
+    directives: {  },
+
+    data() {
+        return {
+            
+        };
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
